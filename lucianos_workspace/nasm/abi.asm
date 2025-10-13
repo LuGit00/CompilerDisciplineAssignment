f47dabcb1c0ae56f0dd6b5550819881cc9acdd3a
Luciano Machado <luciano.abs.machado@gmail.com>
2025-10-13 19:38:08 -0300

    update

--- lucianos_workspace/nasm/abi.asm
+++ lucianos_workspace/nasm/abi.asm
@@ -1,17 +1,19 @@
-; scopes: global, function, struct
-
-
+%push global
+section .bss
+vstack: resb 16*1024*1024
+section .text
+lea rbp,[rel vstack]
+xor rsp,rsp
 
 %macro function 1-*
 	%ifnctx global
 		%fatal
 	%endif
-
 	%push %??
 	%assign %$stack.variables.count 0
 	%assign %$stack.total 0
-	%assign %$frame.base %[%rsp]
 	%1:
+	mov r12,rsp
 	section .text
 %endmacro
 
@@ -19,29 +21,48 @@
 	%ifnctx function
 		%fatal
 	%endif
-
-	lea rax, %%return_address
-    mov [rbp+rsp], rax
-    add rsp, 8
-    jmp %1
-%%return_address:
+	lea rax,[rel %%ret%=]
+	%assign __argc %0-1
+	%if __argc > 0
+		%assign __i 2
+		%rep __argc
+			mov rdx,%[%__i]
+			mov [rbp+rsp],rdx
+			add rsp,8
+			%assign __i __i+1
+		%endrep
+	%endif
+	mov [rbp+rsp],r12
+	add rsp,8
+	mov [rbp+rsp],rax
+	add rsp,8
+	%if __argc > 0
+		lea rsi,[rbp+rsp-16-8*__argc]
+		mov rdi,__argc
+	%else
+		lea rsi,[rbp+rsp-16]
+		xor rdi,rdi
+	%endif
+	jmp %1
+%%ret%=:
 %endmacro
+
 
 %macro return 0
 	%ifnctx function
 		%fatal
 	%endif
-
-	sub rsp, 8
-    mov rax, [rbp+rsp]
-    jmp rax
+	sub rsp,8
+	mov rax, [rbp+rsp]
+	sub rsp,8
+	mov r12,[rbp+rsp]
+	jmp rax
 %endmacro
 
 %macro label 1-*
 	%ifnctx function
 		%fatal
 	%endif
-
 	%rep %0
 		%1:
 		%rotate 1
@@ -52,7 +73,6 @@
 	%ifnctx function
 		%fatal
 	%endif
-
 	jmp %1
 %endmacro
 
@@ -60,7 +80,6 @@
 	%ifnctx function
 		%fatal
 	%endif
-
 	mov rax, %1
 %endmacro
 
@@ -76,12 +95,12 @@
 	%elifctx function
 		%rep %0 - 1
 			%assign %$stack.variables.count %$stack.variables.count + %[size]
-			%xdefine %1 [rsp - %$stack.variables.count]
+			%xdefine %1 [rbp+r12+%$stack.variables.count-%[size]]
 			%rotate 1
 		%endrep
 	%elifctx struct
 		%rep %0 - 1
-			.%1 resb %[size] 
+			.%1 resb %[size]
 			%rotate 1
 		%endrep
 	%else
@@ -92,13 +111,13 @@
 
 %macro struct 1
 	%push %??
-
 	struc %1
 %endmacro
 
 %macro assign 1
-	mov qword [%1], rax
+	mov qword %1, rax
 %endmacro
+
 %macro add 1
 	add rax, %1
 %endmacro
@@ -109,48 +128,66 @@
 	imul rax, %1
 %endmacro
 %macro divide 1
-	mov rcx, %1
+	mov rcx,%1
 	cqo
 	idiv rcx
 %endmacro
+
 %macro if_less_than 1
 	cmp rax, %1
+	setl al
+	movzx rax, al
 %endmacro
 %macro if_more_than 1
 	cmp rax, %1
+	setg al
+	movzx rax, al
 %endmacro
 %macro if_less_or_equal_than 1
 	cmp rax, %1
+	setle al
+	movzx rax, al
 %endmacro
 %macro if_more_or_equal_than 1
 	cmp rax, %1
+	setge al
+	movzx rax, al
 %endmacro
 %macro if_equals 1
 	cmp rax, %1
+	sete al
+	movzx rax, al
 %endmacro
 %macro if_not_equals 1
 	cmp rax, %1
+	setne al
+	movzx rax, al
 %endmacro
 
 %macro if 0
+	%ifndef %$ifseq
+		%assign %$ifseq 0
+	%endif
+	%assign %$ifseq %$ifseq+1
+	%xdefine %$if_else .if_else%$ifseq
+	%xdefine %$if_end  .if_end%$ifseq
+	test rax, rax
+	jz %$if_else
 	%push function
 %endmacro
-%macro then 0
-	%pop
-	%push function
-%endmacro
+
 %macro else 0
 	%pop
+	jmp %$if_end
+%$if_else:
 	%push function
 %endmacro
 
 %macro end 0
-	%ifctx function
-	%elifctx struct
+	%ifctx struct
 		endstruc
-	%else
-		%fatal
 	%endif
+%$if_end:
 	%pop
 %endmacro
 
@@ -160,5 +197,3 @@
 		%rotate 1
 	%endrep
 %endmacro
-
-%push global
--- lucianos_workspace/nasm/main.asm
+++ lucianos_workspace/nasm/main.asm
@@ -1,90 +1,90 @@
 %include "abi.asm"
 
-include io, lib
-
-namespace simple_list
-private
-	namespace list_node
-	private
-		struct list_node
-			var value, next
-		end
-	public
-		function create, value
-			sizeof list_node
-			execute malloc
-			var self
-			assign self
-			load value
-			assign ; self->value
-			load 0
-			assign ; self->next
-			return self
-		end
-	end
-	struct simple_list
-		var value, next
-	end
-public
-	function create
-		sizeof simple_list
-		execute malloc
-		var self
-		assign self
-		load 0
-		assign ; self->head
-		load 0
-		assign ; self->tail
-		return self
-	end
-	function append, self, value
-		execute list_node.create
-		var node
-		assign node
-		load value
-		assign ; node->value
-		if
-			load ; self->tail
-		then
-			load node
-			assign ; self->tail->next
-		else
-			load node
-			assign ; self->head
-		end
-		load node
-		assign ; self->tail
-	end
-end
-
 function main
-	execute simple_list.create
-	var list0
-	assign list0
-	block
-		var index
-		load 0
-		assign index
-		while
-			load index
-			less_than 100
-		do
-			execute simple_list.append list0, index
-			load index
-			add 1
-		end
-	end
-	block
-		var node
-		load ; list0->head
-		while
-			load node
-		do
-			execute lib.print_int ; node->value
-			load ; node->next
-			assign node
-		end
-	end
+	var 8, head, heap_next
+	var 4096, heap
+	immediate 0
+	assign head
+	execute push, head, 1
+	execute push, head, 2
+	execute push, head, 3
+	execute print, head
+	asm "mov rax,60"
+	asm "xor rdi,rdi"
+	asm "syscall"
 end
 
-%include "abi.asm"
+function alloc
+	var 8, p, base, next
+	immediate heap
+	assign base
+	immediate [heap_next]
+	assign next
+	immediate [base]
+	add next
+	assign p
+	immediate p
+	asm "mov rax,[rax]"
+	return
+end
+
+function push
+	var 8, hdr, val, node, tmp
+	immediate [rsi]
+	assign hdr
+	immediate [rsi+8]
+	assign val
+	execute alloc
+	asm "mov [node],rax"
+	immediate [hdr]
+	asm "mov rdx,rax"
+	asm "mov rdx,[rdx]"
+	immediate [node]
+	asm "mov rax,rax"
+	asm "mov [rax],rdx"
+	immediate [val]
+	asm "mov rdx,rax"
+	immediate [node]
+	add 8
+	asm "mov rax,rax"
+	asm "mov [rax],rdx"
+	immediate [hdr]
+	asm "mov rcx,rax"
+	immediate [node]
+	asm "mov rax,rax"
+	asm "mov [rcx],rax"
+	return
+end
+
+function print
+	var 8, hdr, cur, ch
+	immediate [rsi]
+	assign hdr
+	immediate [hdr]
+	asm "mov rdx,rax"
+	asm "mov rdx,[rdx]"
+	asm "mov [cur],rdx"
+label loop
+	asm "mov rax,[cur]"
+	if_equals 0
+	if
+		goto done
+	end
+	asm "mov rax,[cur]"
+	add 8
+	asm "mov rax,[rax]"
+	add 48
+	assign ch
+	immediate ch
+	asm "mov rsi,rax"
+	asm "mov rax,1"
+	asm "mov rdi,1"
+	asm "mov rdx,1"
+	asm "syscall"
+	asm "mov rax,[cur]"
+	asm "mov rax,[rax]"
+	asm "mov [cur],rax"
+	goto loop
+label done
+	return
+end
