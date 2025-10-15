db6a72aee36bc42992c68a00689df19fb2ef5d1e
Luciano Machado <luciano.abs.machado@gmail.com>
2025-10-15 12:58:23 -0300

    update

--- lucianos_workspace/nasm/abi.asm
+++ lucianos_workspace/nasm/abi.asm
@@ -28,21 +28,79 @@ system.registers.saved_callee.initialize rbx, rbp, r12, r13, r14, r15
 %undef system.registers.saved_callee.initialize
 
 
-; Initialize global scope
-%push global
-%xdefine %$storage_class undefined
-%xdefine %$type undefined
 
-%macro struct 1-*
-	%push %??
-	%xdefine %$storage_class undefined
-	%xdefine %$type undefined
+
+
+
+
+
+
+
+
+%assign _scope.depth 0
+%macro _scope.open 1
+	%push %1
+	%xdefine %$access_modifier private
+	%xdefine %$storage_class auto
+	%xdefine %$type void
+	%assign %$variables.count 0
+	%if %[_scope.depth] > 0
+		%rep %[%$$variables.count]
+			%xdefine %$variables.%[%$variables.count] %[%$$variables.%[%$variables.count]]
+			%xdefine %$variables.%[%$variables.count].access_modifier %[%$$variables.%[%$variables.count].access_modifier]
+			%xdefine %$variables.%[%$variables.count].storage_class %[%$$variables.%[%$variables.count].storage_class]
+			%xdefine %$variables.%[%$variables.count].type %[%$$variables.%[%$variables.count].type]
+			%assign %$variables.count %[%$variables.count] + 1
+		%endrep
+	%endif
+	%assign %$labels.count 0
+	%if %[_scope.depth] > 0
+		%rep %[%$$labels.count]
+			%xdefine %$labels.%[%$labels.count] %[%$$labels.%[%$labels.count]]
+			%assign %$labels.count %[%$labels.count] + 1
+		%endrep
+	%endif
+	%assign _scope.depth %[_scope.depth] + 1
+%endmacro
+%macro _scope.close 0
+	%assign _scope.depth %[_scope.depth] - 1
+	%rep %[%$labels.count]
+		%undef %$labels.%[%$labels.count]
+		%assign %$labels.count %[%$labels.count] - 1
+	%endrep
+	%undef %$labels.count
+	%rep %[%$variables.count]
+		%undef %$variables.%[%$variables.count].type
+		%undef %$variables.%[%$variables.count].storage_class
+		%undef %$variables.%[%$variables.count].access_modifier
+		%undef %$variables.%[%$variables.count]
+		%assign %$variables.count %[%$variables.count] - 1
+	%endrep
+	%undef %$variables.count
+	%undef %$type
+	%undef %$storage_class
+	%undef %$access_modifier
+	%pop
 %endmacro
 
-%macro function 1-*
-	%push %??
-	%xdefine %$storage_class undefined
-	%xdefine %$type undefined
+_scope.open global
+
+
+
+
+
+
+
+%macro private 0
+	%xdefine %$access_modifier %??
+%endmacro
+
+%macro protected 0
+	%xdefine %$access_modifier %??
+%endmacro
+
+%macro public 0
+	%xdefine %$access_modifier %??
 %endmacro
 
 %macro register 0
@@ -55,6 +113,10 @@ system.registers.saved_callee.initialize rbx, rbp, r12, r13, r14, r15
 
 %macro static 0
 	%xdefine %$storage_class %??
+%endmacro
+
+%macro void 0
+	%xdefine %$type %??
 %endmacro
 
 %macro uint8_t 0
@@ -73,9 +135,7 @@ system.registers.saved_callee.initialize rbx, rbp, r12, r13, r14, r15
 	%xdefine %$type %??
 %endmacro
 
-%macro var 2-*
-	%assign size %1
-	%rotate 1
+%macro var 1-*
 	%assign counter 0
 	%rep %0 - 1
 
@@ -89,14 +149,40 @@ system.registers.saved_callee.initialize rbx, rbp, r12, r13, r14, r15
 		%assign counter %[counter] + 1
 	%endrep
 	%undef counter
-	%undef size
 
-	; Reset symbol table buffers
-	%xdefine %$storage_class undefined
-	%xdefine %$type undefined
+	%xdefine %$access_modifier private
+	%xdefine %$storage_class auto
+	%xdefine %$type void
 
 %endmacro
 
+%macro struct 1-*
+	_scope.open %??
+	struc %1
+%endmacro
+
+%macro function 1-*
+	_scope.open %??
+	section .text.%1
+	%1:
+%endmacro
+
+%macro end 0-*
+	%ifctx global
+		%fatal
+	%elifctx struct
+		%fatal
+	%elifctx function
+		%fatal
+	%else
+		%fatal
+	%endif
+
+	%xdefine %$access_modifier private
+	%xdefine %$storage_class auto
+	%xdefine %$type void
+
+%endmacro
 
 
 
